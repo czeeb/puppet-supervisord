a03dff29f09412a75fe69d0947b4c388a11e3358
Chris Zeeb <chris.zeeb@gmail.com>
2013-05-29 17:37:14 -0400

    Removing vagrant build in environment.  This repository should always have been a submodule to a vagrant environment for building puppet modules.  This way the vagrant specific files stay out of the puppet module itself, keeping it pristine.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,48 @@
-package { 'vim':
-  ensure   => 'installed',
-}
-
-package { 'git':
-  ensure   => 'installed',
-}
-
-package { 'rake':
-  ensure   => 'installed',
-  provider => 'gem',
-}
-
-package { 'puppet':
-  ensure   => 'installed',
-  provider => 'gem',
-}
-
-package { 'rspec-puppet':
-  ensure   => 'installed',
-  provider => 'gem',
-}
-
-package { 'puppet-lint':
-  ensure   => 'installed',
-  provider => 'gem',
-}
-
-package { 'puppetlabs-spec-helper':
-  ensure   => 'installed',
-  provider => 'gem',
-  source   => "/vagrant/puppetlabs_spec_helper-0.4.1.3.gem"
+# == Class: supervisord
+#
+# A module to manage supervisord and the services it runs.
+#
+# === Parameters
+# [*version*]
+# The package version to install
+#
+# [*enable*]
+# Should the service be enabled at boot time?
+#
+# [*start*]
+# Should the service be started by Puppet
+#
+# === Authors
+#
+# Chris Zeeb <chris.zeeb@gmail.com>
+#
+# === Copyright
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+class supervisord (
+  $version       = 'present',
+  $enable        = true,
+  $start         = true,
+  $logfile       = '/var/log/supervisor/supervisord.log',
+  $pidfile       = '/var/run/supervisord.pid',
+  $childlogdir   = '/var/log/supervisor',
+  $serverurl     = 'unix:///var/run/supervisor.sock',
+  $include_files = ['/etc/supervisor/conf.d/*.conf']
+) {
+  class{'supervisord::install': } ->
+  class{'supervisord::config': } ~>
+  class{'supervisord::service': } ->
+  Class['supervisord']
 }
