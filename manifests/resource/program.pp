956d79850485772638104d3b6b1a316ef8324841
Chris Zeeb <chris.zeeb@gmail.com>
2013-10-01 22:06:52 -0400

    More documentation updates.

--- manifests/resource/program.pp
+++ manifests/resource/program.pp
@@ -3,15 +3,58 @@
 # This definition creates a program config file
 #
 # Parameters:
-#
+# $ensure                  - 
+# $command                 - REQUIRED - command to execute
+# $process_name            - The name for each instance started with this
+#                            config
+# $numprocs                - The number of instances to start with the command
+# $numprocs_start          - The number to begin incrementing from for
+#                            numprocs
+# $priority                - The priority to set this config at
+# $autostart               - Sets if program should start when supervisor
+#                            is started
+# $autorestart             - Behaviour for restarting program when it exits
+# $startsecs               - Number of seconds program must run to be
+#                            considered successful 
+# $startretries            - Number of restart retries before program is set
+#                            to a fatal state 
+# $exitcodes               - List of expected exit codes (non-error) 
+# $stopsignal              - Signal to send program when stop is requested 
+# $stopwaitsecs            - Number of seconds to wait for the OS to return
+#                            a SIGCHILD to supervisord after program has sent
+#                            a stop signal 
+# $stopasgroup             - Set if supervisor sends a stop signal to entire
+#                            group. 
+# $killasgroup             - Set if SIGKILL sent to entire process group
+# $user                    - The user the program runs as 
+# $redirect_stderr         - If true cause the process' stderr output to be
+#                            sent back to supervisor on its stdout file
+#                            descriptor 
+# $stdout_logfile          - 
+# $stdout_logfile_maxbytes - 
+# $stdout_logfile_backups  -
+# $stdout_capture_maxbytes - 
+# $stdout_events_enabled   - 
+# $stderr_logfile          - 
+# $stderr_logfile_maxbytes - 
+# $stderr_logfile_backups  - 
+# $stderr_capture_maxbytes - 
+# $stderr_events_enabled   -
+# $environment             -  
+# $directory               -  
+# $umask                   - 
+# $serverurl               - 
 #
 # Actions:
+# - Installs supervisor program config files
 #
 # Requires:
+# - The supervisord class
 #
 # Sample Usage:
 # supervisord::resource::program { 'helloworld':
-#   ensure => present,
+#   ensure  => present,
+#   command => '/bin/cat',
 # }
 
 define supervisord::resource::program (
